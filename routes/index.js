337e8a96edb9e4c7a19b097e3be0916ef3fd29d8
JeremyM02 <zom7@fresnou.org>
2022-01-19 08:54:52 -0800

    Reached Slide 30 again?

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 const productController = require('../controllers/productController.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 router.get('/products/profile/:id', productController.renderProfile);
 router.get('/products/edit/:id', productController.renderEditForm);
+router.post('/products/edit:id', productController.updateProduct);
 module.exports = router;

b46f4f1f612c1a4350c7619cd4da796a6478bf79
JeremyM02 <zom7@fresnou.org>
2022-01-20 08:55:55 -0800

    Reached Slide 54

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const productController = require('../controllers/productController.js');
+const reviewController = require('../controllers/reviewController.js');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.redirect('/products');
@@ -12,4 +13,6 @@ router.post('/products/edit/:id', productController.updateProduct);
 router.get('/products/add', productController.renderAddForm);
 router.post('/products/add', productController.addProduct);
 router.get('/products/delete/:id', productController.deleteProduct);
+
+router.get('products/:productId/review/add', reviewController.renderAddReviewForm);
 module.exports = router;
